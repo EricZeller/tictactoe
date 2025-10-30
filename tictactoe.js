0711dc26ccc09dfe3d78ab1bc0dfc06ccc8fd478
EricZeller <98104805+EricZeller@users.noreply.github.com>
2025-10-30 21:12:59 +0100

    css mulitplayer fix

--- tictactoe.js
+++ tictactoe.js
@@ -486,3 +486,44 @@ function closeRoom() {
     }
 }
 
+
+// Dark Theme// check for saved "lightMode" in localStorage
+let lightMode = localStorage.getItem("lightMode");
+
+const lightModeToggle = document.querySelector("#icon");
+
+const enablelightMode = () => {
+    // 1. Add the class to the body
+    document.body.classList.add("lightMode");
+    icon.src = "moon.png"
+    // 2. Update lightMode in localStorage
+    localStorage.setItem("lightMode", "enabled");
+}
+
+const disablelightMode = () => {
+    // 1. Remove the class from the body
+    document.body.classList.remove("lightMode");
+    icon.src = "sun.png"
+    // 2. Update lightMode in localStorage 
+    localStorage.setItem("lightMode", null);
+}
+
+// If the user already visited and enabled lightMode
+// start things off with it on
+if (lightMode === "enabled") {
+    enablelightMode();
+}
+
+// When someone clicks the button
+lightModeToggle.addEventListener("click", () => {
+    // get their lightMode setting
+    lightMode = localStorage.getItem("lightMode");
+
+    // if it not current enabled, enable it
+    if (lightMode !== "enabled") {
+        enablelightMode();
+        // if it has been enabled, turn it off  
+    } else {
+        disablelightMode();
+    }
+});
